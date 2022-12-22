1e3fd0c05cd0f067aa8acf5861f94c64a7d47982
RutbelCarlosTC <90486158+RutbelCarlosTC@users.noreply.github.com>
2022-12-21 19:48:51 -0500

    se crea funcion para obtener lista de articulos

--- cgi-bin/list.pl
+++ cgi-bin/list.pl
@@ -1,1 +1,30 @@
 #!/usr/bin/perl
+use strict;
+use warnings;
+use CGI;
+use DBI;
+
+my $q = CGI->new();
+print $q->header(-type => "text/xml", -charset => "utf-8");
+my $owner = $q->param("owner");
+
+
+sub getArticulos{
+  my $owner = $_[0];
+  my $user = 'alumno';
+  my $paswd = 'pweb1';
+  my $dsn = 'DBI:MariaDB:database=pweb1;host=localhost';
+  my $dbh = DBI->connect($dsn, $user, $paswd) or die("No se pudo conectar!");
+
+  my $sql = "SELECT Title FROM Articles WHERE owner = ?";
+
+  my $sth = $dbh->prepare($sql);
+  $sth->execute($owner);
+  my @articulos=();
+  while(my @row = $sth->fetchrow_array){
+    push(@articulos,$row[0]);
+  }
+  $sth->finish;
+  $dbh->disconnect;
+  return @articulos;
+}
