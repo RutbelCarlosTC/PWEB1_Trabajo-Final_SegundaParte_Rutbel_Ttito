772d455bd1507de8055f9bff2e69cc72d4bb5b69
RutbelCarlosTC <90486158+RutbelCarlosTC@users.noreply.github.com>
2022-12-21 19:37:09 -0500

    funcion para obtner el marckdown de un articulo

--- cgi-bin/article.pl
+++ cgi-bin/article.pl
@@ -1,1 +1,61 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+use CGI;
+use DBI;
+
+my $q = CGI->new; 
+print $q->header(-type => "text/xml", -charset => "utf-8");
+my $owner = $q->param("owner");
+my $title = $q->param("title");
+
+my $contenido;
+if(defined($title) && defined($text) && defined($owner)){
+  my $flag =insertarArticulo($title,$text,$owner);
+  if($flag){
+    my @campos = ("title","text");
+    my %xml = (
+      $campos[0] =>$title,
+      $campos[1] =>$text,
+    );
+    $contenido = renderContenido(\@campos,\%xml);
+  }
+}
+printXML("article",$contenido);
+
+sub getText{
+  my ($title,$owner) = ($_[0],$_[1]);
+  my $user = "alumno";
+  my $paswd = "pweb1";
+  my $dsn = "DBI:MariaDB:database=pweb1;host=localhost";
+  my $dbh = DBI->connect($dsn, $user, $paswd) or die("No se pudo conectar!");
+
+  my $sql = "SELECT Text FROM Articles WHERE Title=? AND Owner =?";
+  my $sth = $dbh->prepare($sql);
+  $sth->execute($title,$owner);
+  my @row = $sth->fetchrow_array; 
+  my $text = $row[0]; 
+  $sth->finish;
+  $dbh->disconnect;
+  return $text;
+}
+
+sub printXML{
+  my $principal = $_[0];
+  my $contenido  = $_[1];
+  print "<$principal>\n";
+  if(defined($contenido)){
+    print $contenido;
+  }
+  print "</$principal>\n";
+}
+sub renderContenido{
+  my @claves =@{$_[0]};
+  my %campos =%{$_[1]};
+  my $str = "";
+  foreach my $key (@claves){
+    $str .= "  <$key>".$campos{$key}."</$key>\n";
+  }
+  return $str;
+}
 
