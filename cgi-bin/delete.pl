798a5f621f21a2cbc15e2f0157b83d328c8e2140
RutbelCarlosTC <90486158+RutbelCarlosTC@users.noreply.github.com>
2022-12-21 19:54:04 -0500

    modificando metodo actulizar por borrar articulo

--- cgi-bin/delete.pl
+++ cgi-bin/delete.pl
@@ -7,34 +7,18 @@ use DBI;
 my $q = CGI->new; 
 print $q->header(-type => "text/xml", -charset => "utf-8");
 my $title = $q->param("title");
-my $text = $q->param("text");
 my $owner = $q->param("owner");
 
-my $contenido;
-if(defined($title) && defined($owner)){
-  my $flag =actualizarArticulo($title,$text,$owner);
-  if($flag !=0){
-    my @campos = ("title","text");
-    my %xml = (
-      $campos[0] =>$title,
-      $campos[1] =>$text,
-    );
-    $contenido = renderContenido(\@campos,\%xml);
-  }
-}
-
-printXML("article",$contenido);
-
-sub actualizarArticulo{
-  my ($title,$text,$owner) = ($_[0],$_[1],$_[2]);
-  my $user = 'alumno';
-  my $paswd = 'pweb1';
-  my $dsn = 'DBI:MariaDB:database=pweb1;host=localhost';
+sub borrarArticulo{
+  my ($title,$owner) = ($_[0],$_[1]);
+  my $user = "alumno";
+  my $paswd = "pweb1";
+  my $dsn = "DBI:MariaDB:database=pweb1;host=localhost";
   my $dbh = DBI->connect($dsn, $user, $paswd) or die("No se pudo conectar!");
 
-  my $sql = "UPDATE Articles SET Text =? WHERE Title=? AND Owner =?";
+  my $sql = "DELETE FROM Articles WHERE Title=? AND Owner =?";
   my $sth = $dbh->prepare($sql);
-  my $flag = $sth->execute($text,$title,$owner);
+  my $flag =$sth->execute($title,$owner);
   $sth->finish;
   $dbh->disconnect;
   return $flag;
