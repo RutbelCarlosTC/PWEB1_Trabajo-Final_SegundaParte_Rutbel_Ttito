bd6b7c3411a6bc38f0561580c4902445660e7df7
RutbelCarlosTC <90486158+RutbelCarlosTC@users.noreply.github.com>
2022-12-21 19:25:55 -0500

    verificando parametros de entrada e imprime xml

--- cgi-bin/register.pl
+++ cgi-bin/register.pl
@@ -12,6 +12,20 @@ my $password = $q->param("password");
 my $firstName = $q->param("firstName");
 my $lastName = $q->param("lastName");
 
+if(defined($userName) && defined($password) && defined($firstName) && defined($lastName)){
+  my $flag =registerUser($userName,$password,$firstName,$lastName);
+  if(defined($flag)){
+    my @campos = ("owner","firstName","lastName");
+    my %xml = (
+      $campos[0] =>$userName,
+      $campos[1] =>$firstName,
+      $campos[2] =>$lastName,
+    );
+    $contenido = renderContenido(\@campos,\%xml);
+  }
+}
+printXML("user",$contenido);
+
 sub registerUser{
   my $userName = $_[0];
   my $password = $_[1];
